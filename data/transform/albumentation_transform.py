fb9c2388487d2f4b7694a41bd0e68230f74c2dba
Shashank-Holla <58994044+Shashank-Holla@users.noreply.github.com>
2021-07-10 01:28:38 +0530

    changes as part of LR finder module addition

--- data/transform/albumentation_transform.py
+++ data/transform/albumentation_transform.py
@@ -9,11 +9,11 @@ def albumentation_transform(train, mean, stddev):
         transform = A.Compose([ 
                            A.Normalize(mean=mean, std=stddev, max_pixel_value=255.0), 
                            A.PadIfNeeded(min_height=36, min_width=36, value=mean, always_apply=True),
-                           A.RandomCrop (height=32, width=32, always_apply=True, p=1.0),
-                        #    A.HorizontalFlip(always_apply=False, p=0.5),
+                           A.RandomCrop(height=32, width=32, always_apply=True, p=1.0),
+                           A.HorizontalFlip(always_apply=False, p=0.5),
                         #    A.ShiftScaleRotate (shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
-                           A.CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1, fill_value=mean),
-                           A.Rotate(limit=5, always_apply=True, p=1.0),
+                           A.CoarseDropout(max_holes=1, max_height=8, max_width=8, min_holes=1, fill_value=mean),
+                           # A.Rotate(limit=5, always_apply=True, p=1.0),
                         #    A.ToGray(),
                            ToTensorV2(),
                            ])
